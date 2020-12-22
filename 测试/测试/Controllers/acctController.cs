19f33317a416870170070bb1087a46ba3b3fe014
8bxf8 <2213489838@qq.com>
2020-12-22 12:06:11 +0800

    测试连接

---
+++ 测试/测试/Controllers/acctController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 测试.Controllers
+{
+    public class acctController : Controller
+    {
+        // GET: acct
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
